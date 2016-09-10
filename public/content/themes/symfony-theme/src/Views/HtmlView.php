b480515c03c235334b5488bd921114dfcedc0a79
Rolf den Hartog <rolf@rolfdenhartog.nl>
2016-09-10 10:18:47 +0200

    Added a method to simply load an asset in the method of a controller: `$view->loadAsset('/assets/styles/some-stylesheet.css')`

--- public/content/themes/symfony-theme/src/Views/HtmlView.php
+++ public/content/themes/symfony-theme/src/Views/HtmlView.php
@@ -8,6 +8,32 @@ namespace SymfonyTheme\Views;
 class HtmlView extends AbstractView
 {
     /**
+     * Array with stylesheets to load.
+     *
+     * @var array
+     */
+    protected $styles = [];
+
+    /**
+     * Array with scripts to load.
+     *
+     * @var array
+     */
+    protected $scripts = [];
+
+    /**
+     * @param string $file
+     */
+    public function loadAsset($file)
+    {
+        if (pathinfo($file, PATHINFO_EXTENSION) === 'css') {
+            $this->styles[str_slug($file)] = $file;
+        } else {
+            $this->scripts[str_slug($file)] = $file;
+        }
+    }
+
+    /**
      * Renders a full page.
      *
      * @param  string $template
@@ -16,6 +42,18 @@ class HtmlView extends AbstractView
      */
     public function renderPage($template, array $data = [])
     {
+        add_action('wp_enqueue_scripts', function () {
+            // styles
+            foreach ($this->styles as $handle => $script) {
+                wp_enqueue_style($handle, asset($script));
+            }
+
+            // scripts
+            foreach ($this->scripts as $handle => $script) {
+                wp_enqueue_script($handle, asset($script), ['jquery']);
+            }
+        });
+
         $html = $this->renderTemplate('/layout', array_merge($data, [
             'content' => $this->renderTemplate($template, $data),
         ]));
